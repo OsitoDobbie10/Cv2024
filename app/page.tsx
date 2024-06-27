eed01d3890cd80b4996f1a0af5e8c2b04d42819f
ositodobbie10 <caamledzeppelin@gmail.com>
2024-06-27 15:48:34 -0600

    "Contenido"

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
+import Container from "@/components/Container";
 import CoverParticles from "@/components/CoverParticles";
 import Introduction from "@/components/Introduction";
 import TransitionPages from "@/components/TransitionPages";
 export default function Home() {
   return (
-   <main>
+   <Container>
     <TransitionPages/>
     <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradiant-cover">
     <CoverParticles/>
     <Introduction/>
     </div>
-   </main>
+   </Container>
   );
 }
