764eeb16192fbabdcfa3924ee4b9beea63dbbf3a
ositodobbie10 <caamledzeppelin@gmail.com>
2024-06-28 11:47:04 -0600

    'Cambio imagen'

--- components/AvatarServices.tsx
+++ components/AvatarServices.tsx
@@ -5,7 +5,7 @@ import Data from "@/public/dato.png"
 const AvatarServices = () => {
   return (
     <TransitionComponent position='right' className="bottom-0 left-0 hidden md:inline-block md:absolute">
-    <Image src={Data} alt="Data" width={200} height={200} className="w-[350px] h-full"/>
+    <Image src={Data} alt="Data" width={150} height={150} className="w-[150px] h-full"/>
     </TransitionComponent>
   )
 }
